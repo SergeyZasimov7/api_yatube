75464bd4c9ca35943f5d737a06bc2b3605733de7
Сергей Засимов <zasimovsergey@yandex.ru>
2024-05-21 10:01:49 +0300

    3

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,14 +1,14 @@
+from rest_framework import permissions
 from rest_framework import serializers
-from rest_framework.permissions import BasePermission
 
 from posts.models import Comment, Group, Post
 
 
-class IsAuthorOrReadOnly(BasePermission):
+class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in ['PUT', 'PATCH', 'DELETE']:
-            return obj.author == request.user
-        return True
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -37,4 +37,4 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ['post']
+        read_only_fields = ('post',)
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,5 @@
-from rest_framework import status, viewsets
-from rest_framework.decorators import action
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from posts.models import Comment, Group, Post
 from api.serializers import (CommentSerializer, GroupSerializer,
@@ -13,23 +11,6 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
-    @action(detail=True, methods=['get', 'post'])
-    def comments(self, request, pk=None):
-        post = self.get_object()
-        if request.method == 'GET':
-            comments = post.comments.all()
-            serializer = CommentSerializer(comments, many=True)
-            return Response(serializer.data)
-        elif request.method == 'POST':
-            serializer = CommentSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save(author=request.user, post=post)
-                return Response(serializer.data, status.HTTP_201_CREATED)
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
@@ -40,6 +21,16 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_pk')
+        if post_id:
+            return Comment.objects.filter(post_id=post_id)
+        return Comment.objects.none()
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_pk')
+        post = Post.objects.get(pk=post_id)
+        serializer.save(author=self.request.user, post=post)
