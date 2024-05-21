f967e3923decf740a0f158c83cbb2ede0bc39bea
Сергей Засимов <zasimovsergey@yandex.ru>
2024-05-21 12:38:38 +0300

    5

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
-from rest_framework import status, viewsets
-from rest_framework.exceptions import NotFound
+
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from posts.models import Group, Post
 from api.serializers import (CommentSerializer, GroupSerializer,
@@ -27,22 +27,11 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def get_queryset(self):
-        if self.kwargs.get('post_pk') is None:
-            raise NotFound("Post not found.")
         return self.get_post().comments.all()
 
     def get_post(self):
-        post_id = self.kwargs.get('post_pk')
-        try:
-            return Post.objects.get(pk=post_id)
-        except Post.DoesNotExist:
-            return None
+        return get_object_or_404(Post, pk=self.kwargs.get('post_pk'))
 
     def perform_create(self, serializer):
         post = self.get_post()
-        if post:
-            serializer.save(author=self.request.user, post=post)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(
-            {"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND
-        )
+        serializer.save(author=self.request.user, post=post)
